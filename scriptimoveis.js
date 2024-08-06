0c54c5dca92556d4f35be59eba4845f5249146c2
Joseilda Nascimento <joseildatn@gmail.com>
2024-08-06 17:05:04 -0300

    alterações

--- scriptimoveis.js
+++ scriptimoveis.js
@@ -17,10 +17,7 @@ const swiper = new Swiper('.swiper', {
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
-  },
-  autoplay: {
-    delay: 3000,
-  },
+
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
